dc5ba0a0ace69ef7dfc9a85efe97a1c90ad90d43
Andrejs Grocs <andrey.detochkin@gmail.com>
2022-04-01 13:34:35 +0200

    frontend base + some functionality

--- Frontend_Project/src/App.js
+++ Frontend_Project/src/App.js
@@ -1,10 +1,44 @@
 import React from 'react'
 import MainRouter from './hoc/MainRouter'
+import { createContext, useEffect, useState } from "react";
+
+
+export const AppContext = createContext({
+  username: "",
+  loggedIn: false,
+  handleLogin: () => {},
+});
+
+
+
+
 
 export default function App() {
+
+    const [user, setUser] = useState({});
+    const [loggedIn, setLoggedIn] = useState("");
+
+    
+      const handleLogin = (_user) => {
+    if (_user) {
+      setUser(_user);
+      setLoggedIn(true);
+     
+    } else {
+      setUser("");
+      setLoggedIn(false);
+    }
+
+  };
+
+
     return (
-        <div>
-            <MainRouter />
-        </div>
+    <AppContext.Provider
+      value={{ handleLogin, loggedIn, user}}
+    >
+      <div>
+        <MainRouter />
+      </div>
+    </AppContext.Provider>
     )
 }
